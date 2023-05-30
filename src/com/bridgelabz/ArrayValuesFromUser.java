433dd790979dcc5d4bb1c9c5fd98a8c5e87a188d
philo73 <philips.dayan@gmail.com>
2023-05-30 23:48:50 +0530

    using Buffer Reader

--- src/com/bridgelabz/ArrayValuesFromUser.java
+++ src/com/bridgelabz/ArrayValuesFromUser.java
@@ -1,42 +1,54 @@
 package com.bridgelabz;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class ArrayValuesFromUser {
-    static ArrayList<Integer> arraylist = new ArrayList<Integer>();
+    static ArrayList<Integer> arraylist = new ArrayList<>();
+    static InputStreamReader input = new InputStreamReader(System.in);
+    static BufferedReader reader = new BufferedReader(input);
     static boolean flag = true;
-    static Scanner objSC = new Scanner(System.in);
+   // static Scanner objSC = new Scanner(System.in);
     static String yORno;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
 //        userRequest();
 
         for(int i=0; flag ;i++) {
-            userRequest();
+//            userRequest();
+            System.out.print("do you want to add values to the array? Y/N:  ");
+            yORno = reader.readLine();
 
             if(yORno.equals("Y")||yORno.equals("y")) {
-                addValue();
-                System.out.println("input value : "+yORno);
+//                addValue();
+//                System.out.println("input value : "+yORno);
                 //userRequest();
+                System.out.print("Enter values : " );
+                arraylist.add(Integer.parseInt(reader.readLine()));
 
-            } else {
+            } else if((yORno.equals("N")||yORno.equals("n"))) {
                 flag=false;
+            }
+            else {
+                System.out.println("enter valid input");
+                flag=true;
+
             }
         }
         System.out.println("Values in Array : "+arraylist);
     }
-    public static void addValue(){
-        System.out.print("Enter values : " );
-        arraylist.add(objSC.nextInt());
-        }
-
-    public static void userRequest() {
-
-        System.out.print("do you want to add values to the array? Y/N:  ");
-        yORno = objSC.nextLine();
-        System.out.println("User input is ======"+yORno);
-    }
+//    public static void addValue() {
+//
+//        }
+//
+//    public static void userRequest() {
+//
+////        System.out.print("do you want to add values to the array? Y/N:  ");
+////        yORno = reader.readLine();
+//        System.out.println("User input is ======"+yORno);
+//    }
 
     }
 
